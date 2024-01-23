ab71ff8693df9d32edd1f6502ee2d51206052a25
Ricardo Lázaro <caffeine305@gmail.com>
2024-01-23 13:13:58 -0600

    Enjambre listo y destruible

--- Assets/Scripts/Beehaviour.cs
+++ Assets/Scripts/Beehaviour.cs
@@ -5,26 +5,72 @@ using UnityEngine;
 public class Beehaviour : MonoBehaviour
 {
     public float enemySpeed;
-    float randomAngle;
+    public float randomAngle;
+    float cadence;
     public Rigidbody thisRigidbody;
     private Vector2 direccion;
 
     void Start()
     {
-        enemySpeed = 10.0f;
+        enemySpeed = 8.0f;
+        cadence = 0.05f;
+        InvokeRepeating("ChangeAngle",0f,cadence);
         //thisRigidbody = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
+        enemySpeed = Random.Range(10.0f,13.0f); 
         transform.Translate(Vector2.right * enemySpeed * Time.deltaTime);
-        randomAngle = Random.Range(-45.0f,45.0f);
+        
         transform.Rotate(0.0f, 0.0f, randomAngle, Space.World);
         //cube1.transform.Rotate(xAngle, yAngle, zAngle, Space.Self);
 
-        if(transform.position.x < -25.0f || transform.position.x > 21.0f)
+        if(transform.position.x < -9.5f || transform.position.x > 9.5f)
             Destroy(transform.root.gameObject);
+        
+        if(transform.position.y < -5.5f || transform.position.y > 5.5f)
+            Destroy(transform.root.gameObject);
+
+        OnTouchDestroy();
+
     }
+
+    void OnTouchDestroy()
+    {
+        if (Input.touchCount == 1)
+        {
+            Vector3 wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+            Vector2 touchPos = new Vector2(wp.x, wp.y);
+
+            if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos))
+            {
+                Destroy(this.transform.gameObject);
+            }
+        }
+
+    }
+
+    void ChangeAngle()
+    {
+        randomAngle = Random.Range(-30.0f,30.0f);
+    }
+
+    
+    void OnMouseDown()
+    {
+        this.gameObject.SetActive(false);
+        //GameManager.instanceGameManager.PointsManager(points);
+        //loadSound.destroySound();
+        Destroy(this.gameObject, 1.0f);
+
+    }
+    
+
+
+
+
+
 
 
 }
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject bee;
+    public float cadence;
+    public Transform origen;
+    private GameObject instance_bee;
+     void Awake()
+    {
+        cadence = 1.2f;
+        InvokeRepeating("InstanceBees",0f,cadence);
+        origen = this.transform;
+    }
+
+    private void InstanceBees()
+    {
+        instance_bee = Instantiate(bee, origen.position,Quaternion.identity);
+        //instance_bullet.transform.right = origen.forward;
+        //instance_bee.GetComponent<Rigidbody>().AddForce(origen.forward * bulletForce, ForceMode.Impulse );
+    }
+}
