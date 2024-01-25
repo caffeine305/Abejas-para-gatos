b2e89994b923b42cfe22f49702f19432969a7d9d
Ricardo Lázaro <caffeine305@gmail.com>
2024-01-24 19:13:36 -0600

    Update Spawner.cs
    Sólo se varió cadencia y se quitó el flag no definido que identificará el sentido del movimiento del Spawner Transform

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -10,7 +10,7 @@ public class Spawner : MonoBehaviour
     private GameObject instance_bee;
      void Awake()
     {
-        cadence = 1.2f;
+        cadence = 0.8f;
         InvokeRepeating("InstanceBees",0f,cadence);
         origen = this.transform;
     }
@@ -20,5 +20,10 @@ public class Spawner : MonoBehaviour
         instance_bee = Instantiate(bee, origen.position,Quaternion.identity);
         //instance_bullet.transform.right = origen.forward;
         //instance_bee.GetComponent<Rigidbody>().AddForce(origen.forward * bulletForce, ForceMode.Impulse );
+    }
+
+    private void MoveSidetoSide()
+    {
+        //if moveRight
     }
 }
